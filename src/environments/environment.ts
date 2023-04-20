b0b70b826294fa8da25270ba7cb4af03fd7e0bab
Tairinee <tayrine.pvh@gmail.com>
2023-04-20 11:48:00 -0300

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'kanbanfire-67992',
+    appId: '1:673277541251:web:ad5d8e06989b98077154b6',
+    storageBucket: 'kanbanfire-67992.appspot.com',
+    apiKey: 'AIzaSyC9-BusvpWNHCa_oi5TCBFU7sihwVMYb-s',
+    authDomain: 'kanbanfire-67992.firebaseapp.com',
+    messagingSenderId: '673277541251',
+    measurementId: 'G-Y80ERHQX0K',
+  }
+};
